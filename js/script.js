8df23ae0e14a2d87c0afd7216fc919e319777ba1
mrr <mrr@technopark.ru>
2017-09-26 16:36:34 +0300

    m-5 t-1 is done

--- js/script.js
+++ js/script.js
@@ -3,23 +3,25 @@
 var expandControls = document.querySelectorAll('.expand-control');
 
 var hidePopups = function() {
-  [].forEach.call(document.querySelectorAll('.expand-list'), function(item) {
-    item.classList.add('hidden');
-  });
+    [].forEach.call(document.querySelectorAll('.expand-list'), function(item) {
+        item.classList.add('hidden');
+    });
 };
 
 document.body.addEventListener('click', hidePopups, true);
 
 [].forEach.call(expandControls, function(item) {
-  item.addEventListener('click', function() {
-    item.nextElementSibling.classList.toggle('hidden');
-  });
+    item.addEventListener('click', function() {
+        item.nextElementSibling.classList.toggle('hidden');
+    });
 });
 
-var $checkbox = document.getElementsByClassName('checkbox__input')[0];
+var $checkbox = document.getElementById('show-complete-tasks');
 
-$checkbox.addEventListener('change', function(event) {
-  var is_checked = +event.target.checked;
+if ($checkbox) {
+    $checkbox.addEventListener('change', function(event) {
+        var is_checked = +event.target.checked;
 
-  window.location = '/index.php?show_completed=' + is_checked;
-});
+        window.location = '/index.php?show_completed=' + is_checked;
+    });
+}
