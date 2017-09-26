c4aa1c6a5838aedb98ede4e8452befe5fdd77ab9
mrr <mrr@technopark.ru>
2017-09-26 17:43:31 +0300

    m-7 t-1 is done

---
+++ init.php
@@ -0,0 +1,8 @@
+<?php
+$connect = mysqli_connect('localhost', 'root', '', 'doingsdone');
+if ($connect == false) {
+    $error = render_template('templates/error.php', ['error' => mysqli_connect_error()]);
+    print($error);
+    exit();
+}
+?>
